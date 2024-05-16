cbebbe644e87c4a40a317cff94b92de71fa4f9aa
Sharon <sasimanju1975@gmail.com>
2024-05-17 00:15:02 +0530

    third commit

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=2h-6ol7vi(!7ua*)^$2kp1e!(9fdjy!pvpzi*w_rh@g0^lj%7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1:8000','127.0.0.1']
 
 
 # Application definition
